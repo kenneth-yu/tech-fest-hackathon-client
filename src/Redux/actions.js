6e68b7b2948bb7679e66d29d35afd5e737a1e79c
kenneth-yu <kyu22@buffalo.edu>
2019-04-06 19:43:01 -0400

    getting token back from login

--- src/Redux/actions.js
+++ src/Redux/actions.js
@@ -1,22 +1,22 @@
 export function authenticateUser(username, password) {
   return (dispatch) => {
-    let user = {username: username, password: password}
+    let user = {email: username, password: password}
     console.log(user)
     return fetch(`http://localhost:3000/authenticate`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        Accept: 'application/json',
+        Accept: 'application/json'
       },
-      body: JSON.stringify({user})
+      body: JSON.stringify({email: username, password: password})
     }).then(res => res.json())
     .then(data =>{
       if (data.message){
         window.alert(data.message)
       }
       else{
-        console.log(data)
-        // localStorage.setItem("token", data.jwt)
+        // console.log(data.auth_token)
+        localStorage.setItem("token", data.auth_token)
         // dispatch({type: "AUTHENTICATE_USER", payload: data.user});
       }
     }).catch(error => console.log(error))
